79176e78021005d9c8f62329805c27375380cda8
Brian Sukhnandan <brian@Brians-MacBook-Air.local>
2021-03-29 01:10:17 -0400

    added hammerspoon config for macOS

---
+++ hammerspoon/init.lua
@@ -0,0 +1,121 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Q", function()
+	hs.alert.show("Fuck you motherfucker!!")
+end)
+
+-- * Left
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+	local win = hs.window.focusedWindow()
+	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	f.x = max.x
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+	win:setFrame(f)
+end)
+
+-- * Right
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+	local win = hs.window.focusedWindow()
+ 	local f = win:frame()
+	local screen = win:screen()
+	local max = screen:frame()
+
+	f.x = max.x + (max.w / 2)
+	f.y = max.y
+	f.w = max.w / 2
+	f.h = max.h
+	win:setFrame(f)
+end)
+
+-- * Top-Left
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+	local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+end)
+
+-- * Bottom-Left
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+end)
+
+-- * Top-Right
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "O", function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+end)
+
+-- * Bottom-Right
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ".", function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+end)
+
+--* Top-Half
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h / 2
+        win:setFrame(f)
+end)
+
+--* Bottom-Half
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ",", function()
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w
+        f.h = max.h / 2
+        win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+	hs.reload()
+end)
+hs.alert.show("Reloaded Hammerspoon Config.")
+
